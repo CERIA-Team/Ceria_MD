1609e39948ce9a51f7d5dc0ab42849b9edff5e68
Christian Gary Trisanto <102272678+sugarygary@users.noreply.github.com>
2024-06-13 17:56:25 +0700

    Finished player feature

--- app/src/main/java/com/ceria/capstone/ui/listening/ListeningFragment.kt
+++ app/src/main/java/com/ceria/capstone/ui/listening/ListeningFragment.kt
@@ -1,23 +1,146 @@
 package com.ceria.capstone.ui.listening
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.SeekBar
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.ceria.capstone.BuildConfig
 import com.ceria.capstone.R
 import com.ceria.capstone.databinding.FragmentListeningBinding
 import com.ceria.capstone.ui.common.BaseFragment
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.types.Track
+import timber.log.Timber
+import java.util.Locale
+import java.util.Timer
+import java.util.TimerTask
 
-class ListeningFragment : BaseFragment<FragmentListeningBinding>(FragmentListeningBinding::inflate) {
-
+class ListeningFragment :
+    BaseFragment<FragmentListeningBinding>(FragmentListeningBinding::inflate) {
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+    private var playbackPositionTimer: Timer? = null
+    private val uiHandler = Handler(Looper.getMainLooper())
+    private var isUserSeeking = false
+    private val debounceHandler = Handler(Looper.getMainLooper())
+    private var debounceRunnable: Runnable? = null
     override fun setupUI() {
+        with(binding) {
+            val connectionParams = ConnectionParams.Builder(BuildConfig.SPOTIFY_CLIENT_ID)
+                .setRedirectUri("ceriaauthresponse://callback").showAuthView(false).build()
 
+            SpotifyAppRemote.connect(
+                requireContext(),
+                connectionParams,
+                object : Connector.ConnectionListener {
+                    override fun onConnected(appRemote: SpotifyAppRemote) {
+                        spotifyAppRemote = appRemote
+                        Timber.d("Connected! Yay!")
+                        spotifyAppRemote?.let { remote ->
+                            val playlistURI = "spotify:playlist:37i9dQZF1DX4JAvHpjipBk"
+                            remote.playerApi.play(playlistURI)
+                            remote.playerApi.subscribeToPlayerState().setEventCallback {
+                                val track: Track = it.track
+                                titlealbum.text = track.name
+                                titlealbum.isSelected = true
+                                groupalbum.text =
+                                    track.artists.joinToString(separator = ", ") { artist -> artist.name }
+                                groupalbum.isSelected = true
+                                textbpm2.isSelected = true
+                                Glide.with(requireContext()).load(
+                                    track.imageUri.raw?.replace(
+                                        "spotify:image:", "https://i.scdn.co/image/"
+                                    )
+                                ).into(imagelistening)
+                                timeend.text = formatMilliseconds(track.duration)
+                                seekbar.max = (track.duration / 1000).toInt()
+                                if (!isUserSeeking) {
+                                    seekbar.progress = (it.playbackPosition / 1000).toInt()
+                                }
+                                if (it.isPaused) {
+                                    imageplaysong.setImageResource(R.drawable.play_icon)
+                                } else {
+                                    imageplaysong.setImageResource(R.drawable.pause_icon)
+                                }
+                                Timber.d(track.name + " by " + track.artist.name)
+                            }
+                        }
+                    }
+
+                    override fun onFailure(throwable: Throwable) {
+                        Timber.e("Failed to connect: " + throwable.message, throwable)
+                    }
+                })
+        }
     }
 
     override fun setupListeners() {
-        binding.stopsession.setOnClickListener {
-            findNavController().navigate(R.id.summaryFragment)
+        with(binding) {
+            seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?, progress: Int, fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        spotifyAppRemote?.playerApi?.seekTo(progress * 1000L)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    isUserSeeking = true
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    debounceRunnable?.let { debounceHandler.removeCallbacks(it) }
+                    debounceRunnable = Runnable {
+                        isUserSeeking = false
+                    }
+                    debounceHandler.postDelayed(debounceRunnable!!, 1200)
+                }
+            })
+            stopsession.setOnClickListener {
+                findNavController().navigate(R.id.summaryFragment)
+            }
+            imageplaysong.setOnClickListener {
+                spotifyAppRemote?.playerApi?.playerState?.setResultCallback { playerState ->
+                    if (playerState.isPaused) {
+                        spotifyAppRemote?.playerApi?.resume()
+                    } else {
+                        spotifyAppRemote?.playerApi?.pause()
+                    }
+                }?.setErrorCallback { throwable ->
+
+                }
+            }
+            imagenextsong.setOnClickListener {
+                spotifyAppRemote?.playerApi?.skipNext()
+            }
+            imagepresong.setOnClickListener {
+                spotifyAppRemote?.playerApi?.skipPrevious()
+            }
         }
     }
 
     override fun setupObservers() {
-
+        with(binding) {
+            playbackPositionTimer = Timer()
+            playbackPositionTimer?.schedule(object : TimerTask() {
+                override fun run() {
+                    uiHandler.post {
+                        spotifyAppRemote?.playerApi?.playerState?.setResultCallback { playerState ->
+                            val position = playerState.playbackPosition
+                            timestart.text = formatMilliseconds(position)
+                            if (!isUserSeeking) {
+                                seekbar.progress = (playerState.playbackPosition / 1000).toInt()
+                            }
+                        }
+                    }
+                }
+            }, 0, 1000)
+        }
     }
+
+    private fun formatMilliseconds(ms: Long): String =
+        String.format(Locale.getDefault(), "%02d:%02d", (ms / 1000) / 60, (ms / 1000) % 60)
 }
